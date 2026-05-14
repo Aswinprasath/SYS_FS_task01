239dde7a65aee734ad6df06d798515bb0bcaa9c3
Aswin Prasath <gowthamginju9524@gmail.com>
2026-05-14 21:22:59 +0530

    Header bar created with elements

---
+++ public/script.js
@@ -0,0 +1,11 @@
+
+
+const labelone = document.getElementById("header-lab2");
+
+labelone.addEventListener("mouseover" , () => {
+    labelone.innerText = "Explore > ";
+});
+
+labelone.addEventListener("mouseout" , () => {
+    labelone.innerText = "Explore ^ ";
+});
