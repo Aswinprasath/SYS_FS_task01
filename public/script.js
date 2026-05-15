3e6c10972422d49f4ff5af053812f35a5d022695
Aswin Prasath <gowthamginju9524@gmail.com>
2026-05-15 16:25:48 +0530

    Updated Program- Vedio added

--- public/script.js
+++ public/script.js
@@ -9,3 +9,37 @@ labelone.addEventListener("mouseover" , () => {
 labelone.addEventListener("mouseout" , () => {
     labelone.innerText = "Explore ▼ ";
 });
+
+
+// const showfea = () => {
+//     document.getElementById("fea-elements").style.display= "block";
+// }
+
+const elementsfea = document.getElementById("fea-elements");
+
+const headerlab1 = document.getElementById("header-lab1");
+
+headerlab1.addEventListener('click' , () => {
+    // elementsfea.style.display = "block";
+    if( elementsfea.style.display === "none"){
+        elementsfea.style.display = "block";
+    }
+    else{
+        elementsfea.style.display = "none";
+    }
+});
+
+const vediotop = document.getElementById("video-box");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            vediotop.play();
+        }
+        else{
+            vediotop.pause();
+        }
+    });
+});
+
+observer.observe(vediotop)
